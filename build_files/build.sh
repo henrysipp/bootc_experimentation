1b02c3351a30442a075633e94eb3155e381ae121
Henry Sipp <hesipp@gmail.com>
2025-08-04 22:30:47 -0500

    1password install

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,6 +19,10 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+rpm --import https://downloads.1password.com/linux/keys/1password.asc
+sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
+dnf install 1password
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
