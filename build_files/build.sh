59b233054478d4b9a63a8e3a248fc7e99d48c0e1
Henry Sipp <hesipp@gmail.com>
2025-08-12 21:50:52 -0500

    gnome extensions

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
 set -ouex pipefail
-
-
 mkdir -p /var/opt /var/roothome
 
+
+# Plugins
 dnf5 -y install rsync
 dnf5 -y install 'dnf5-command(copr)'
 dnf5 -y install dnf5-plugins
 
 # rsync system files into the container image
 rsync -rvK /ctx/system_files/ /
-# rsync -a /ctx/system_files/usr/ /usr/
 
-# dnf check-update
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-dnf5 -y install gnome-tweaks 
 
 # COPR Repos
 dnf5 -y config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
@@ -38,9 +28,8 @@ dnf5 -y copr disable jdxcode/mise
 rpm --import https://downloads.1password.com/linux/keys/1password.asc
 dnf5 install -y 1password 1password-cli
 
-#### Example for enabling a System Unit File
-
-# Install 1Password
 bash /ctx/build_files/1password.sh
+bash /ctx/build_files/gnome_shell.sh
 
+# Services
 systemctl enable podman.socket
---
+++ build_files/gnome_shell.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+dnf5 -y install gnome-tweaks 
+dnf install -y gnome-extensions-app 
+dnf install -y gnome-shell-extension-blur-my-shell 
+dnf install -y gnome-shell-extension-dash-to-dock 
