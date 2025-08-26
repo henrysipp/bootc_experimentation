e0cde2d8f44e54556dbd871a4094f9dba1daa489
Henry Sipp <hesipp@gmail.com>
2025-08-26 09:57:05 -0500

    Fix?

--- build_files/gnome_shell.sh
+++ build_files/gnome_shell.sh
@@ -6,8 +6,7 @@ dnf5 -y install gnome-shell-extension-appindicator \
     gnome-shell-extension-blur-my-shell \
     gnome-shell-extension-dash-to-dock \
     gnome-shell-extension-hotedge \
-    gnome-shell-extension-auto-move-windows \
-    gnome-shell-extension-paperwm
+    gnome-shell-extension-auto-move-windows
 
 # Compile schemas to ensure they're valid after GNOME shell setup
 echo "Compiling schemas after GNOME shell setup..."
