96868715e5d2aad2c58498dbcaa5d1480ca17092
Henry Sipp <hesipp@gmail.com>
2025-11-10 23:31:39 -0600

    huh

--- build_files/build.sh
+++ build_files/build.sh
@@ -23,17 +23,17 @@ dnf5 -y install podman podman-compose
 dnf5 -y install git
 dnf5 -y install gh --repo gh-cli
 dnf5 -y install tmux 
-dnf5 -y install code
+# dnf5 -y install code
 
 # Dependencies for erlang mise install, probably useful for others
 dnf5 -y install gcc gcc-c++ make autoconf automake openssl-devel ncurses-devel
 
-dnf5 install fedora-workstation-repositories
-dnf5 config-manager setopt google-chrome.enabled=1
+# dnf5 install fedora-workstation-repositories
+# dnf5 config-manager setopt google-chrome.enabled=1
 # # dnf5 config-manager --set-enabled google-chrome
-dnf5 -y install google-chrome-stable
+# dnf5 -y install google-chrome-stable
 
-dnf5 config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+dnf5\ config-manager addrepo --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
 dnf5 -y install tailscale
 dnf5 -y install mise
 dnf5 -y install cifs-utils
