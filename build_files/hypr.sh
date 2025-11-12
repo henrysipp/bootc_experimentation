25dc36cf2399cc9dd9d8c745d4d0466e5fa34da8
Henry Sipp <hesipp@gmail.com>
2025-11-12 12:59:12 -0500

    hypr script

--- build_files/hypr.sh
+++ build_files/hypr.sh
@@ -1,5 +1,84 @@
 #!/bin/bash
+set -e
 
-dnf5  -y copr enable solopasha/hyprland
-dnf5 -y install hyprland hyprpaper hyprlock hypridle waybar kitty xdg-desktop-portal-hyprland
-# dnf5 -y install hyprland-devel # If you want to build plugins (use hyprpm)com/
+# Enable COPR repo
+dnf5 -y copr enable solopasha/hyprland
+
+# --- Base Hyprland setup ---
+dnf5 -y install \
+  hyprland \
+  hyprpaper \
+  hyprlock \
+  hypridle \
+  waybar \
+  kitty \
+  xdg-desktop-portal-hyprland
+
+# --- Core utilities ---
+dnf5 -y install \
+  fzf \
+  polkit \
+  xfce-polkit \
+  dbus-tools \
+  dbus-daemon \
+  wl-clipboard \
+  pavucontrol \
+  playerctl \
+  qt5-qtwayland \
+  qt6-qtwayland \
+  gnome-disk-utility \
+  openssl \
+  vim \
+  just \
+  alsa-firmware
+
+# --- Sound ---
+dnf5 -y install \
+  wireplumber \
+  pipewire \
+  pamixer \
+  pulseaudio-utils
+
+# --- Networking ---
+dnf5 -y install \
+  network-manager-applet \
+  NetworkManager-openvpn \
+  NetworkManager-openvpn-gnome \
+  NetworkManager-openconnect \
+  NetworkManager-openconnect-gnome \
+  bluez \
+  bluez-tools \
+  blueman \
+  firewall-config
+
+# --- File management ---
+dnf5 -y install \
+  thunar \
+  thunar-archive-plugin \
+  thunar-volman \
+  xarchiver \
+  imv \
+  p7zip \
+  gvfs-mtp \
+  gvfs-gphoto2 \
+  gvfs-smb \
+  gvfs-nfs \
+  gvfs-fuse \
+  gvfs-archive \
+  android-tools
+
+# --- Screenshot tools ---
+dnf5 -y install \
+  slurp \
+  grim
+
+# --- Display management ---
+dnf5 -y install \
+  wlr-randr \
+  wlsunset \
+  brightnessctl \
+  kanshi
+
+# --- Notifications ---
+dnf5 -y install \
+  dunst
