108663d6230b9ff75c6f0f702c208add4cb5029e
Henry Sipp <hesipp@gmail.com>
2025-11-12 12:54:51 -0500

    More updates

--- build_files/hypr.sh
+++ build_files/hypr.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-dnf5 -y install hyprland
-# dnf5 -y install hyprland-devel # If you want to build plugins (use hyprpm)
+dnf5  -y copr enable solopasha/hyprland
+dnf5 -y install hyprland hyprpaper hyprlock hypridle waybar kitty xdg-desktop-portal-hyprland
+# dnf5 -y install hyprland-devel # If you want to build plugins (use hyprpm)com/
